da7caa14e118ccad1c67661be2a29908b0e08e5e
yaohejian <1228441292@qq.com>
2023-02-23 18:28:20 +0800

    1.GridView 2.GridLayout

--- app/src/main/java/com/winfo/mypractise/MainActivity.java
+++ app/src/main/java/com/winfo/mypractise/MainActivity.java
@@ -3,6 +3,8 @@ package com.winfo.mypractise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GridView gridview=findViewById(R.id.gridview);
     }
 }
